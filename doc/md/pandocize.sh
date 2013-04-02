f79eb2f3737770e7bc2595ff696b80afffc53ae0
Romain Perier <rperier@sierrawireless.com>
2013-04-02 15:32:52 +0200

    Adding agent_connector_libraries/Airvantage_Lua_library.md documentation

--- doc/md/pandocize.sh
+++ doc/md/pandocize.sh
@@ -29,6 +29,7 @@ markdown_list="
     agent/Using_treemgr_handlers_for_asset_management.md
     agent/Remote_Script.md
     agent/Monitoring.md
+    agent_connector_libraries/Airvantage_Lua_library.md
     utilitary_libraries/Lua_RPC.md
     utilitary_libraries/Modbus.md
     utilitary_libraries/Serialize_Deserialize_Lua_objects.md
