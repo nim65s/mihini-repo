1e1ed713398d5a09bc363421faeb7bc2721814a3
Romain Perier <rperier@sierrawireless.com>
2013-04-15 15:43:17 +0200

    Don't generate a version "Build: Unknown" when building out-of a git tree

--- agent/executable/version.c
+++ agent/executable/version.c
@@ -11,15 +11,13 @@
 
 #include "version.h"
 
-
-// If the SVN Revision is known
-#ifndef GIT_REV
-    #define GIT_REV "Unknown"
-#endif //not GIT_REV
-
 int luaopen_agent_versions(lua_State *L)
 {
+#ifdef GIT_REV
     lua_pushstring(L, MIHINI_AGENT__MAJOR_VERSION "." MIHINI_AGENT__MINOR_VERSION " - Build: " GIT_REV);
+#else
+    lua_pushstring(L, MIHINI_AGENT__MAJOR_VERSION "." MIHINI_AGENT__MINOR_VERSION);
+#endif
     lua_setglobal(L, "_MIHINI_AGENT_RELEASE");
     lua_pushstring(L, LUA_RELEASE);
     lua_setglobal(L, "_LUARELEASE");
