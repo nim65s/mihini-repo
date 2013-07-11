2835ffa8dde4f6191a564ba4f1f48c484863f39e
Romain Perier <rperier@sierrawireless.com>
2013-07-11 14:29:02 +0200

    Porting sys_test to returncodes

--- cfwk/system/sys_test.c
+++ cfwk/system/sys_test.c
@@ -16,11 +16,11 @@
 
 #define CALL_TEST(call)                \
 do {                                    \
-  swi_status_t res;                \
+  rc_ReturnCode_t res;                \
                           \
   res = call;                                \
-  SWI_LOG("DT_TEST", (res == SWI_STATUS_OK) ? INFO : ERROR,  #call "...%s\n", (res == SWI_STATUS_OK) ? "OK" : "FAIL"); \
-  if (res != SWI_STATUS_OK)                        \
+  SWI_LOG("DT_TEST", (res == RC_OK) ? INFO : ERROR,  #call "...%s\n", (res == RC_OK) ? "OK" : "FAIL"); \
+  if (res != RC_OK)                        \
     {                                    \
       SWI_LOG("DT_TEST", ERROR, "Test failed with status code %d\n", res); \
       return 1;                                \
@@ -29,26 +29,26 @@ do {                                    \
 
 static int test_sys_Init()
 {
-  swi_status_t res;
+  rc_ReturnCode_t res;
 
   res = swi_sys_Init();
-  return res != SWI_STATUS_OK ? 1 : 0;
+  return res != RC_OK ? 1 : 0;
 }
 
 static int test_sys_Destroy()
 {
-  swi_status_t res;
+  rc_ReturnCode_t res;
 
   res = swi_sys_Destroy();
-  return res != SWI_STATUS_OK ? 1 : 0;
+  return res != RC_OK ? 1 : 0;
 }
 
 static int test_sys_Reboot(const char *reason)
 {
-  swi_status_t res;
+  rc_ReturnCode_t res;
 
   res = swi_sys_Reboot(reason);
-  return res != SWI_STATUS_OK ? 1 : 0;
+  return res != RC_OK ? 1 : 0;
 }
 
 int main(void)
